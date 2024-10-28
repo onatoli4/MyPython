543f809488ad282b82bea75b15547edd6025f2a5
onatoli4 <alek.kostin@gmail.com>
2024-10-28 05:01:19 -0500

    04_ALL

--- exercises/04_data_structures/answer_task_4_8.py
+++ exercises/04_data_structures/answer_task_4_8.py
@@ -24,11 +24,12 @@
 Это не значит, что задание сделано правильно, просто на данном этапе сложно иначе
 проверять результат.
 """
-
 ip = "192.168.3.1"
-ipl = ip.split(".")
-ipt = '''
+
+octets = ip.split(".")
+
+output = """
 {0:<10}{1:<10}{2:<10}{3:<10}
-{0:08b}  {1:08b}  {2:08b}  {3:08b}
-'''
-print(ipt.format(int(ipl[0]), int(ipl[1]), int(ipl[2]), int(ipl[3])))
+{0:08b}  {1:08b}  {2:08b}  {3:08b}"""
+
+print(output.format(int(octets[0]), int(octets[1]), int(octets[2]), int(octets[3])))
