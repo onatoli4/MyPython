865c0b4779dd8dfab727205743bc875147ee419f
onatoli4 <alek.kostin@gmail.com>
2024-10-30 07:29:57 -0500

    Task 6_2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,28 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP-адрес в формате x.x.x.x: ")
+ip_list = ip.split(".")
+ip_valid = True
+
+if len(ip_list) != 4:
+    ip_valid = False
+else:
+    for octet in ip_list:
+        if not octet.isdigit() or int(octet) < 0 or int(octet) > 255:
+            ip_valid = False
+            break
+if ip_valid:
+    ip1 = int(ip_list[0])
+    if ip == "0.0.0.0":
+        print("unassigned")
+    elif ip == "255.255.255.255":
+        print("local broadcast")
+    elif ip1 > 0 and ip1 < 224:
+        print("unicast")
+    elif ip1 > 223 and ip1 < 240:
+        print("multicast")
+    else:
+        print("unused")
+else:           
+    print("Неправильный IP-адрес")
