294da1174250fd460200128c0ce665479f36c84c
onatoli4 <alek.kostin@gmail.com>
2024-11-01 07:18:20 -0500

    Task 7_2

--- exercises/07_files/answer_task_7_2.py
+++ exercises/07_files/answer_task_7_2.py
@@ -40,7 +40,10 @@ interface Ethernet0/3
 
 """
 from sys import argv
-with open(argv[1]) as f:
+
+filename = argv[1]
+
+with open(filename) as f:
     for line in f:
-        if line[0] != "!":
+        if not line.startswith("!"):
             print(line.rstrip())
