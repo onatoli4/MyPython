a359a1aaa17a2a5bab767bcc1ffef2461e0e3f5b
onatoli4 <alek.kostin@gmail.com>
2024-11-18 11:27:21 -0600

    Task 17_3a

--- exercises/17_serialization/answer_task_17_3a.py
+++ exercises/17_serialization/answer_task_17_3a.py
@@ -36,21 +36,22 @@
 в файл topology.yaml. Он понадобится в следующем задании.
 
 """
-import glob
 import yaml
+import glob
 from task_17_3 import parse_sh_cdp_neighbors
 
-def generate_topology_from_cdp(list_of_files, save_to_filename = None):
-    result = {}
-    for file in list_of_files:
-        with open(file) as f:
-            result.update(parse_sh_cdp_neighbors(f.read()))
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    topology = {}
+    for filename in list_of_files:
+        with open(filename) as f:
+            topology.update(parse_sh_cdp_neighbors(f.read()))
     if save_to_filename:
-        with open(save_to_filename, 'w') as f:
-            yaml.dump(result, f)
-    return result
+        with open(save_to_filename, "w") as f_out:
+            yaml.dump(topology, f_out, default_flow_style=False)
+    return topology
 
 
-if __name__ == '__main__':
-    list_of_sh = glob.glob("sh_cdp_n_*")
-    print(generate_topology_from_cdp(list_of_sh, 'new_topo.yaml'))
+if __name__ == "__main__":
+    f_list = glob.glob("sh_cdp_n_*")
+    print(generate_topology_from_cdp(f_list, save_to_filename="topology.yaml"))
