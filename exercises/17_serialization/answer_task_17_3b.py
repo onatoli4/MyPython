47f08eb8ff8a0f8be143400a6268d711924a35a0
onatoli4 <alek.kostin@gmail.com>
2024-11-18 14:02:48 -0600

    Task 17_3b

--- exercises/17_serialization/answer_task_17_3b.py
+++ exercises/17_serialization/answer_task_17_3b.py
@@ -46,17 +46,20 @@
 import yaml
 from draw_network_graph import draw_topology
 
-def transform_topology(filename):
-    result = {}
-    with open(filename) as f:
-        topo = yaml.safe_load(f)
-    for l_device, peer in topo.items():
-        for l_port, r_con in peer.items():
-            r_device, r_port = list(r_con.items())[0]
-            if (r_device, r_port) not in result:
-                result[(l_device, l_port)] = (r_device, r_port)
-    return(result)
 
-if __name__ == '__main__':
-    topology = transform_topology('topology.yaml')
-    draw_topology(topology)
+def transform_topology(topology_filename):
+    with open(topology_filename) as f:
+        raw_topology = yaml.safe_load(f)
+
+    formatted_topology = {}
+    for l_device, peer in raw_topology.items():
+        for l_int, remote in peer.items():
+            r_device, r_int = list(remote.items())[0]
+            if not (r_device, r_int) in formatted_topology:
+                formatted_topology[(l_device, l_int)] = (r_device, r_int)
+    return formatted_topology
+
+
+if __name__ == "__main__":
+    formatted_topology = transform_topology("topology.yaml")
+    draw_topology(formatted_topology)
